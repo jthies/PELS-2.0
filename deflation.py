fix several smaller and bigger errors in deflation code and tests. The tests are now running but fail with numerical errors (incorrect results and/or tests)

deflation.py
@@ -12,11 +12,11 @@ import numpy as np
 import scipy
 import scipy.sparse.linalg as spla
 from time import perf_counter
-from numba import cuda, jit
+from numba import cuda, jit, int32, float64
 import pymetis
 
-from kernels import to_device, clone, axpby, spmv
-from cuda_deflation import cu_restrict, cu_prolongate
+from kernels import to_device, from_device, clone, axpby, spmv
+from cuda_deflation import cu_restrict, cu_prolongate, cu_sell_restrict
 
 from sellcs import sellcs_matrix
 
@@ -80,26 +80,29 @@ def partition_csr_matrix(A_csr, nparts):
     return np.array(part, dtype='int32')
 
 @jit(nopython=True)
-def invert_partitioning(nparts, part):
+def invert_partitioning(nparts: int32, part: int32[:]) -> (int32[:], int32[:]):
     r'''
-    if part contains integers in the range 0 to nparts-1, inclusive,
-    creates a column-major 2D array ipart[npart, max_i(part==i)] s.t.
-    ipart[p, j] == i <-> part[i] = p and count(part[0:i]==p)=j.
+    ipart, nmembers = invert_partitioning(nparts, part)
+
+    If part contains integers in the range 0 to nparts-1, inclusive,
+    creates a row-major 2D array ipart[npart, max_i(part==i)] s.t.
+    ipart[p, j] == i <-> part[i] == p and count(part[0:i]==p)=j.
     The second array returned indicates the actual number of global indicesj assigned to partition
     p, nmembers[p]<=ipart.shape[1]. Elements ipart[p,nmembers[p]:] are invalid (undefined).
     '''
     N = part.size
     # First count the partition sizes
-    nmembers=np.zeros(nparts,dtype='int32')
+    nmembers = np.zeros(nparts,dtype='int32')
     for i in range(N):
         nmembers[part[i]] += 1
     # Then construct the inverse mapping per partition:
-    ipart = -np.ones(nparts, np.max(nmembers), dtype='int32')
+    max_k = np.max(nmembers)
+    ipart = -np.ones((nparts, max_k), dtype='int32')
     nmembers[:] = 0
 
-    for p in range(N):
+    for i in range(N):
         pi = part[i]
-        ni = nmembers[i]
+        ni = nmembers[pi]
         ipart[pi, ni] = i
         nmembers[pi] += 1
     return ipart, nmembers
@@ -121,30 +124,44 @@ class DeflatedOperator:
             nc (int): Number of coarse partitions (size of coarse problem).
         '''
         t0 = perf_counter()
-        if type(A_csr) is not scipy.sparse.csr_matrix or type(A_gpu) is not sellcs_matrix:
-            raise Exception('DeflatedOperator requires a CSR (host) and SELL-C-sigma (device) matrix')
+        if (type(A_csr) is not scipy.sparse.csr_matrix or
+            type(A_gpu) is not sellcs_matrix or
+            hasattr(A_gpu, 'cu_data') == False):
+            raise Exception(f'DeflatedOperator requires a CSR (host) and SELL-C-sigma (device) matrix.\n'+
+                            f'Found A_csr[{type(A_csr)}] and A_gpu[{type(A_gpu)}].')
+        self.shape = A_csr.shape
+        self.dtype = A_csr.dtype
         self.A = A_gpu
-        self.nc = nc
+        self.nparts = nc
         self.n = A_csr.shape[0]
 
         # 1. Partitioning using METIS
         self.part = partition_csr_matrix(A_csr, nc)
-        self.cu_part = cuda.to_device(self.part)
+        self.cu_part = to_device(self.part)
 
-        # determine the size of each partition
+        #print('part:')
+        #for i in range(self.n):
+        #    print(f'{i}\t{self.part[i]}')
+
+        # determine the size of each partition and the inverse mapping 'ipart'
         self.ipart, self.nmembers = invert_partitioning(self.nparts, self.part)
+
         self.cu_ipart = to_device(self.ipart)
         self.cu_nmembers = to_device(self.nmembers)
 
-        self.valV = 1.0/dble(nmembers[self.part])
+        self.valV = 1.0/self.nmembers.astype(np.float64)
         self.cu_valV = to_device(self.valV)
+
+        print('TROET')
+        print(self.valV)
 
         nchunks = len(self.A.indptr)-1
         C = self.A.C
+        n = self.A.shape[0]
 
-        self.cu_A_c = numba.device_array((self.nc, self.nc), dtype=self.dtype)
+        self.cu_A_c = cuda.device_array((self.nparts, self.nparts), dtype=self.dtype)
 
-        cu_sell_restrict[nchunks, C](self.A.cu_values, self.A.cu_indptr, self.A.cu_indices, n, 
+        cu_sell_restrict[nchunks, C](self.A.cu_data, self.A.cu_indptr, self.A.cu_indices, n, 
                                      self.cu_part, self.cu_valV,self.cu_A_c)
         cuda.synchronize()
 
@@ -163,10 +180,10 @@ class DeflatedOperator:
         with Q = V(V'AV)\V'
         '''
         # coarsen ("restrict"): x_c = V^T@x
-        x_c = cuda.device_array((nc,),dtype='float64')
+        x_c = cuda.device_array((self.nparts,),dtype='float64')
         threadsperblock = 128
-        blockspergrid = self.nc
-        cu_restrict[blockspergrid, threadsperblock](self.cu_ipart, self.cu_part_size, x, x_c)
+        blockspergrid = self.nparts
+        cu_restrict[blockspergrid, threadsperblock](self.cu_ipart, self.cu_nmembers, self.cu_valV, x, x_c)
 
         # solve the projected linear system, y_c = (V^TAV) \ x_c
         y_c = np.linalg.solve(self.L_c.T, np.linalg.solve(self.L_c, from_device(x_c)))
@@ -174,7 +191,7 @@ class DeflatedOperator:
         # interpolate y = V*y_c
         threadsperblock = 256
         blockspergrid = (self.n + threadsperblock - 1) // threadsperblock
-        cu_prolongate[blockspergrid, threadsperblock](self.cu_part, to_device(y_c), y)
+        cu_prolongate[blockspergrid, threadsperblock](self.cu_part, self.valV, to_device(y_c), y)
 
     def proj(self, x, y, tmp):
         r'''

test_deflation.py
@@ -11,11 +11,25 @@
 import unittest
 import pytest
 import numpy as np
+from numpy.linalg import norm
 import scipy.sparse
-from kernels import to_device, to_host, clone, available_gpus
+from kernels import to_device, to_host, clone, available_gpus, spmv
 from deflation import DeflatedOperator, partition_csr_matrix
 from matrix_generator import create_matrix
 from sellcs import sellcs_matrix
+
+from test_kernels import diff_norm
+
+
+def build_numpy_V(A_defl):
+        n = A_defl.shape[0]
+        nc = A_defl.nparts
+        V = np.zeros((n, nc),dtype='float64')
+        for i in range(n):
+            V[i,A_defl.part[i]] = 1.0
+        nmembers = np.sum(V, axis=0, dtype='int32')
+        V = V @ np.diag(1.0/nmembers.astype('float64'))
+        return V
 
 class DeflationTest(unittest.TestCase):
 
@@ -24,16 +38,20 @@ class DeflationTest(unittest.TestCase):
         self.n = 200
         self.nc = 10
         # Create a simple 1D Laplace matrix
-        self.A_csr = scipy.sparse.diags([[-1]*(self.n-1), [2]*self.n, [-1]*(self.n-1)], [-1, 0, 1]).tocsr()
-        self.A_gpu = to_device(sellcs_matrix(self.A_csr))
-        
+        self.A_csr = scipy.sparse.diags([[-1.0]*(self.n-1), [2.0]*self.n, [-1.0]*(self.n-1)], [-1, 0, 1]).tocsr()
+        self.A_gpu = to_device(sellcs_matrix(self.A_csr, C=32, sigma=1))
+
         # Exact solution and RHS
         self.x_ex = np.random.rand(self.n)
         self.b_gpu = to_device(self.A_csr @ self.x_ex)
-        
+        self.v_tmp = clone(self.b_gpu)
+
         # Deflated Operator
         self.A_defl = DeflatedOperator(self.A_csr, self.A_gpu, self.nc)
-        
+
+        # explicitly build the V operator using numpy
+        self.V = build_numpy_V(self.A_defl)
+
         self.eps = 1e-12
 
     def test_partitioning(self):
@@ -49,15 +67,15 @@ class DeflationTest(unittest.TestCase):
         x = to_device(np.random.rand(self.n))
         y = clone(x)
         self.A_defl.applyQ(x, y)
-        
+
         y_host = to_host(y)
         x_host = to_host(x)
-        
+
         # V' A y should match V' x
-        lhs = self.A_defl.V.T @ (self.A_csr @ y_host)
-        rhs = self.A_defl.V.T @ x_host
-        
-        error = np.linalg.norm(lhs - rhs) / np.linalg.norm(rhs)
+        lhs = self.V.T @ (self.A_csr @ y_host)
+        rhs = self.V.T @ x_host
+
+        error = diff_norm(lhs, rhs)
         assert error < self.eps
 
     def test_applyQ_idempotent(self):
@@ -66,33 +84,32 @@ class DeflationTest(unittest.TestCase):
         y1 = clone(x)
         y2 = clone(x)
         temp = clone(x)
-        
+
         # y1 = Q x
         self.A_defl.applyQ(x, y1)
-        
+
         # temp = A (Q x)
-        import kernels
-        kernels.spmv(self.A_gpu, y1, temp)
-        
+        spmv(self.A_gpu, y1, temp)
+
         # y2 = Q (A Q x)
         self.A_defl.applyQ(temp, y2)
-        
-        error = np.linalg.norm(to_host(y1) - to_host(y2)) / np.linalg.norm(to_host(y1))
+
+        error = diff_norm(y1, y2)
         assert error < self.eps
 
     def test_proj_orthogonality(self):
         ''' Test that P = I - QA satisfies V' A P = 0 '''
         x = to_device(np.random.rand(self.n))
         y = clone(x)
-        
+
         # y = (I - QA) x
-        self.A_defl.proj(x, y)
-        
+        self.A_defl.proj(x, y, self.v_tmp)
+
         y_host = to_host(y)
-        
+
         # V' A y should be zero
-        res = self.A_defl.V.T @ (self.A_csr @ y_host)
-        error = np.linalg.norm(res) / np.linalg.norm(to_host(x))
+        res = self.V.T @ (self.A_csr @ y_host)
+        error = norm(res) / norm(to_host(x))
         assert error < self.eps
 
     def test_proj_idempotent(self):
@@ -100,34 +117,38 @@ class DeflationTest(unittest.TestCase):
         x = to_device(np.random.rand(self.n))
         y1 = clone(x)
         y2 = clone(x)
-        
+
         # y1 = P x
-        self.A_defl.proj(x, y1)
-        
+        self.A_defl.proj(x, y1, self.v_tmp)
+
         # y2 = P (P x)
-        self.A_defl.proj(y1, y2)
-        
-        error = np.linalg.norm(to_host(y1) - to_host(y2)) / np.linalg.norm(to_host(y1))
+        self.A_defl.proj(y1, y2, self.v_tmp)
+
+        error = diff_norm(y1,y2)
         assert error < self.eps
 
-@pytest.mark.parametrize('Matrix', ['Ddiag13', 'Dtest33'])
+@pytest.mark.parametrize('Matrix', ['Ddiag13', 'Dsprandn388'])
 def test_deflation_parametrized(Matrix):
     import scipy.io
     A_csr = scipy.sparse.csr_matrix(scipy.io.mmread('matrices/'+Matrix+'.mm.gz'))
     n = A_csr.shape[0]
     nc = min(n // 2, 5) # Small nc for small matrices
-    
-    A_gpu = to_device(A_csr)
+
+    A_sell = sellcs_matrix(A_csr, C=32, sigma=1)
+
+    A_gpu = to_device(A_sell)
     A_defl = DeflatedOperator(A_csr, A_gpu, nc)
-    
+    V = build_numpy_V(A_defl)
+
     x = to_device(np.random.rand(n))
     y = clone(x)
-    A_defl.proj(x, y)
-    
+    v_tmp = clone(x)
+    A_defl.proj(x, y, v_tmp)
+
     # Orthogonality check: V' A P x = 0
     y_host = to_host(y)
-    res = A_defl.V.T @ (A_csr @ y_host)
-    assert np.linalg.norm(res) / np.linalg.norm(to_host(x)) < 1e-10
+    res = V.T @ (A_csr @ y_host)
+    assert norm(res) / norm(to_host(x)) < 1e-10
 
 if __name__ == '__main__':
     unittest.main()